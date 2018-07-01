ee215c92dbb1f105afa72cfd5cf66cf235daf240
cjie888 <cjie888@163.com>
2018-07-01 19:45:57 +0800

    change fcoin task

--- src/main/java/com/cjie/cryptocurrency/quant/api/fcoin/FcoinUtils.java
+++ src/main/java/com/cjie/cryptocurrency/quant/api/fcoin/FcoinUtils.java
@@ -751,7 +751,7 @@ public class FcoinUtils {
         if (allAsset/2 - ftBalance.getBalance() * marketPrice  > allAsset * increment) {
             BigDecimal amount = new BigDecimal(allAsset/2-ftBalance.getBalance()* marketPrice).setScale(numPrecision, BigDecimal.ROUND_FLOOR);
             //买入
-            //buy(symbol, "limit",  amount, getMarketPrice(marketPrice));//此处不需要重试，让上次去判断余额后重新平衡
+            buy(symbol, "limit",  amount, getMarketPrice(marketPrice));//此处不需要重试，让上次去判断余额后重新平衡
             logger.info("ftbalance:{}, usdtbalance:{}", ftBalance.getBalance() + amount.doubleValue(),
                     usdtBalance.getBalance() + amount.doubleValue() * getMarketPrice(marketPrice).doubleValue());
             logger.info("buy {}, price:{}", amount, marketPrice);
@@ -761,7 +761,7 @@ public class FcoinUtils {
         if (ftBalance.getBalance() * marketPrice - allAsset/2 > allAsset * increment) {
             //卖出
             BigDecimal amount = new BigDecimal(ftBalance.getBalance()* marketPrice-allAsset/2).setScale(numPrecision, BigDecimal.ROUND_FLOOR);
-            //sell(symbol, "limit",  amount, getMarketPrice(marketPrice));//此处不需要重试，让上次去判断余额后重新平衡
+            sell(symbol, "limit",  amount, getMarketPrice(marketPrice));//此处不需要重试，让上次去判断余额后重新平衡
             logger.info("ftbalance:{}, usdtbalance:{}", ftBalance.getBalance() - amount.doubleValue(),
                     usdtBalance.getBalance() + amount.doubleValue() * getMarketPrice(marketPrice).doubleValue());
             logger.info("sell {}, price:{}", amount, marketPrice);
@@ -771,7 +771,7 @@ public class FcoinUtils {
 
     }
 
-        public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
         //getSymbols();
         //getBalance();
         //getPriceInfo("ftusdt");
