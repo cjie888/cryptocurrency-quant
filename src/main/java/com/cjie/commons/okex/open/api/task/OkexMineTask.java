c9ea1bf7ef3e2e68f026d9b087701312631bf2d9
cjie888 <cjie888@163.com>
2018-08-10 13:50:08 +0800

    change kline task

--- src/main/java/com/cjie/commons/okex/open/api/task/OkexMineTask.java
+++ src/main/java/com/cjie/commons/okex/open/api/task/OkexMineTask.java
@@ -33,7 +33,7 @@ public class OkexMineTask {
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
             mineService.mine3("eos", "btc", 0.005, 0.5);
-            mineService.mine3("okb", "usdt", 0.005, 0.7);
+            mineService.mine3("okb", "usdt", 0.005, 0.8);
             mineService.mine3("ltc", "eth", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
