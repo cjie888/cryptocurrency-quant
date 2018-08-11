bb72907dd8cd17eee1b0e29b35a064e33a0ddca5
cjie888 <cjie888@163.com>
2018-08-11 18:32:52 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
@@ -33,9 +33,9 @@ public class MineTask {
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
             mineService.mine3("oktop","eos", "btc", 0.005, 0.5);
-            Thread.sleep(1);
+            Thread.sleep(1000);
             mineService.mine3("oktop","okb", "usdt", 0.005, 0.8);
-            Thread.sleep(1);
+            Thread.sleep(1000);
             mineService.mine3("oktop","bch", "eth", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
