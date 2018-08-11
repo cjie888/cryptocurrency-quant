392bb65653aa25f0b20fae3bb4c8eb89dfb0e07a
cjie888 <cjie888@163.com>
2018-08-11 18:20:38 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
@@ -32,6 +32,8 @@ public class MineTask {
         log.info("start mining");
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
+            mineService.mine3("oktop","eos", "btc", 0.005, 0.5);
+            mineService.mine3("oktop","okb", "usdt", 0.005, 0.8);
             mineService.mine3("oktop","bch", "eth", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/OkexMineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/OkexMineTask.java
@@ -32,8 +32,8 @@ public class OkexMineTask {
         log.info("start mining");
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
-            mineService.mine3("eos", "btc", 0.005, 0.5);
-            mineService.mine3("okb", "usdt", 0.005, 0.8);
+            //mineService.mine3("eos", "btc", 0.005, 0.5);
+            //mineService.mine3("okb", "usdt", 0.005, 0.8);
             mineService.mine3("ltc", "eth", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
