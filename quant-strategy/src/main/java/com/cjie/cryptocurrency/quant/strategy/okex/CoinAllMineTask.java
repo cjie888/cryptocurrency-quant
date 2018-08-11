a19c8b6ce11433d00b2672845d5b77c3635431c5
cjie888 <cjie888@163.com>
2018-08-11 18:49:20 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
@@ -4,8 +4,10 @@ package com.cjie.cryptocurrency.quant.strategy.okex;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.JobExecutionException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
-//@Component
+@Component
 @Slf4j
 public class CoinAllMineTask {
 
@@ -25,12 +27,12 @@ public class CoinAllMineTask {
 
 
     }
-    //@Scheduled(cron = "2 */1 * * * ?")
+    @Scheduled(cron = "*/20 * * * * ?")
     public void mineCurrency3() throws JobExecutionException {
         log.info("start mining");
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
-            mineService.mine3("okb", "usdt", 0.005, 0.7);
+            mineService.mine3("cac", "usdt", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
             log.error("error mining", e);
