025b0fddfba485a6417a67eb5921866c4778f9ce
cjie888 <cjie888@163.com>
2018-08-12 15:44:44 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
@@ -14,7 +14,7 @@ public class CoinAllMineTask {
     @Autowired
     private CoinAllMineService mineService;
 
-    @Scheduled(cron = "*/10 * * * * ?")
+    //@Scheduled(cron = "*/10 * * * * ?")
     public void mineCurrency1() throws JobExecutionException {
         log.info("start mining");
         try {
@@ -26,12 +26,12 @@ public class CoinAllMineTask {
 
 
     }
-    //@Scheduled(cron = "*/20 * * * * ?")
+    @Scheduled(cron = "*/20 * * * * ?")
     public void mineCurrency3() throws JobExecutionException {
         log.info("start mining");
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
-            mineService.mine3("cac", "usdt", 0.005, 0.5);
+            mineService.mine3("cac", "usdt", 0.003, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
             log.error("error mining", e);
