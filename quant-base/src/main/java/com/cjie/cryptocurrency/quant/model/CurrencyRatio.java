5a3fe7fb06acb3d55cc6f44db24f51ebd6795e18
cjie888 <cjie888@163.com>
2018-08-18 11:23:46 +0800

    change taks

--- quant-base/src/main/java/com/cjie/cryptocurrency/quant/model/CurrencyRatio.java
+++ quant-base/src/main/java/com/cjie/cryptocurrency/quant/model/CurrencyRatio.java
@@ -1,7 +1,6 @@
 package com.cjie.cryptocurrency.quant.model;
 
-import lombok.Builder;
-import lombok.Data;
+import lombok.*;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -9,6 +8,8 @@ import java.util.Date;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class CurrencyRatio {
     private Long id;
 
