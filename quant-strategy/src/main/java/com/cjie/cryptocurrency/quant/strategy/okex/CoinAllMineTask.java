5bca378b2ad7133e42508287d63355eb26e385d3
cjie888 <cjie888@163.com>
2018-08-23 17:10:32 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
@@ -14,7 +14,7 @@ public class CoinAllMineTask {
     @Autowired
     private CoinAllMineService mineService;
 
-    @Scheduled(cron = "* */10 * * * ?")
+    //@Scheduled(cron = "* */10 * * * ?")
     public void balance() throws JobExecutionException {
         log.info("start balance");
         try {
