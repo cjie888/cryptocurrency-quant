ef973cf18c2905abdf81c35e1b8327d7c3629de6
cjie888 <cjie888@163.com>
2018-11-06 10:08:17 +0800

    change taks

--- quant-base/src/main/java/com/cjie/cryptocurrency/quant/api/okex/service/spot/impl/SpotProductAPIServiceImpl.java
+++ quant-base/src/main/java/com/cjie/cryptocurrency/quant/api/okex/service/spot/impl/SpotProductAPIServiceImpl.java
@@ -1,5 +1,7 @@
 package com.cjie.cryptocurrency.quant.api.okex.service.spot.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.TypeReference;
 import com.cjie.cryptocurrency.quant.api.okex.bean.spot.result.Ticker;
 import com.cjie.cryptocurrency.quant.api.okex.bean.spot.result.Trade;
 import com.cjie.cryptocurrency.quant.api.okex.bean.spot.result.Book;
@@ -19,9 +21,18 @@ import com.cjie.cryptocurrency.quant.api.okex.service.spot.SpotProductAPIService
 import com.cjie.cryptocurrency.quant.api.okex.bean.spot.result.Ticker;
 import com.cjie.cryptocurrency.quant.api.okex.bean.spot.result.Trade;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.stereotype.Component;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -85,9 +96,22 @@ public class SpotProductAPIServiceImpl extends BaseServiceImpl implements SpotPr
 
     @Override
     public List<String[]> getCandles(String site, final String product_id, final Integer granularity, final String start, final String end) {
-        APIClient apiClient = getSpotProductAPIClient(site);
-        SpotProductAPI spotProductAPI = getSpotProductApi(site, apiClient);
-        return apiClient.executeSync(spotProductAPI.getCandles(product_id, granularity, start, end));
+        MultiValueMap<String, String> headers = new HttpHeaders();
+        headers.add("Referer", "https://www.coinall.com");
+        headers.add("user-agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.84 Safari/537.36");
+        HttpEntity requestEntity = new HttpEntity<>(headers);
+
+        String url = "https://www.okex.com/api/spot/v3/products/" + product_id + "/candles?granularity=60";
+        SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
+        requestFactory.setConnectTimeout(10000);// 设置超时
+        requestFactory.setReadTimeout(10000);
+        RestTemplate client = new RestTemplate(requestFactory);
+
+        client.getMessageConverters().set(1, new StringHttpMessageConverter(StandardCharsets.UTF_8));
+        ResponseEntity<String> response = client.exchange(url, HttpMethod.GET, requestEntity, String.class);
+        String body = response.getBody();
+        log.info(body);
+        return JSON.parseObject(body,new TypeReference<List<String[]>>(){});
     }
 
 }
