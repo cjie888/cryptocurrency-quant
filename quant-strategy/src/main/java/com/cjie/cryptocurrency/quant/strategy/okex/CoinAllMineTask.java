9d800256761d838a7b7256c283c091a776ce550d
cjie888 <cjie888@163.com>
2018-12-04 10:11:10 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/CoinAllMineTask.java
@@ -46,9 +46,9 @@ public class CoinAllMineTask {
         //log.info(JSON.toJSONString(spotAccountAPIService.getAccountByCurrency("btc")));
         try {
             mineService.mine1("cac", "usdt", 0.002);
-            mineService.mine1("pax", "btc", 0.003);
-            mineService.mine1("eth", "btc", 0.002);
-            mineService.mine1("okb", "eth", 0.002);
+            //mineService.mine1("pax", "btc", 0.003);
+           // mineService.mine1("eth", "btc", 0.002);
+           // mineService.mine1("okb", "eth", 0.002);
             //mineService.mine3("cac", "eth", 0.005);
         } catch (Exception e) {
             log.error("error mining", e);
