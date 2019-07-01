d3b3390ce69dbeb2a66b2f1e4c0aeded6a6e753c
cjie888 <cjie888@163.com>
2019-07-01 11:05:10 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/MineTask.java
@@ -57,7 +57,7 @@ public class MineTask {
             //Thread.sleep(1000);
             mineService.mine3("okex","okb", "usdt", 0.003, 0.5);
             mineService.mine3("okexsub1","eth", "usdt", 0.003, 0.5);
-
+            mineService.mine3("okexsub2","btc", "usdt", 0.003, 0.5);
             //Thread.sleep(1000);
             //mineService.mine3("oktop","bch", "eth", 0.005, 0.5);
             //mineService.mine3("cac", "eth", 0.005);
