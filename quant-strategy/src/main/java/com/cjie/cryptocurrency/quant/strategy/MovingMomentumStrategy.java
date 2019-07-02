7380437e7b1c4730f6437908a5eda1fbfdd206b3
cjie888 <cjie888@163.com>
2019-07-02 11:21:10 +0800

    moving momentum strategy

---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MovingMomentumStrategy.java
@@ -0,0 +1,166 @@
+/*******************************************************************************
+ *   The MIT License (MIT)
+ *
+ *   Copyright (c) 2014-2017 Marc de Verdelhan, 2017-2018 Ta4j Organization 
+ *   & respective authors (see AUTHORS)
+ *
+ *   Permission is hereby granted, free of charge, to any person obtaining a copy of
+ *   this software and associated documentation files (the "Software"), to deal in
+ *   the Software without restriction, including without limitation the rights to
+ *   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ *   the Software, and to permit persons to whom the Software is furnished to do so,
+ *   subject to the following conditions:
+ *
+ *   The above copyright notice and this permission notice shall be included in all
+ *   copies or substantial portions of the Software.
+ *
+ *   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ *   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ *   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ *   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ *   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ *   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *******************************************************************************/
+package com.cjie.cryptocurrency.quant.strategy;
+
+import com.alibaba.fastjson.JSON;
+import com.cjie.cryptocurrency.quant.api.okex.service.spot.CurrencyKlineDTO;
+import com.cjie.cryptocurrency.quant.api.okex.service.spot.SpotProductAPIService;
+import com.cxytiandi.elasticjob.annotation.ElasticJobConf;
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.ta4j.core.*;
+import org.ta4j.core.analysis.criteria.TotalProfitCriterion;
+import org.ta4j.core.indicators.EMAIndicator;
+import org.ta4j.core.indicators.MACDIndicator;
+import org.ta4j.core.indicators.StochasticOscillatorKIndicator;
+import org.ta4j.core.indicators.helpers.ClosePriceIndicator;
+import org.ta4j.core.num.PrecisionNum;
+import org.ta4j.core.trading.rules.CrossedDownIndicatorRule;
+import org.ta4j.core.trading.rules.CrossedUpIndicatorRule;
+import org.ta4j.core.trading.rules.OverIndicatorRule;
+import org.ta4j.core.trading.rules.UnderIndicatorRule;
+
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.List;
+
+/**
+ * Moving momentum strategy.
+ * </p>
+ * @see <a href="http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum">
+ *     http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum</a>
+ */
+@ElasticJobConf(name = "collectOkexBalanceJob", cron = "10 */1 * * * ?",
+        description = "获取okex站点余额", eventTraceRdbDataSource = "logDatasource")
+@Slf4j
+public class MovingMomentumStrategy implements SimpleJob {
+
+    private TimeSeries timeSeries;
+
+    private Strategy strategy;
+
+    private TradingRecord tradingRecord;
+
+    @Autowired
+    private SpotProductAPIService spotProductAPIService;
+
+    /**
+     * @param series a time series
+     * @return a moving momentum strategy
+     */
+    public static Strategy buildStrategy(TimeSeries series) {
+        if (series == null) {
+            throw new IllegalArgumentException("Series cannot be null");
+        }
+
+        ClosePriceIndicator closePrice = new ClosePriceIndicator(series);
+        
+        // The bias is bullish when the shorter-moving average moves above the longer moving average.
+        // The bias is bearish when the shorter-moving average moves below the longer moving average.
+        EMAIndicator shortEma = new EMAIndicator(closePrice, 9);
+        EMAIndicator longEma = new EMAIndicator(closePrice, 26);
+
+        StochasticOscillatorKIndicator stochasticOscillK = new StochasticOscillatorKIndicator(series, 14);
+
+        MACDIndicator macd = new MACDIndicator(closePrice, 9, 26);
+        EMAIndicator emaMacd = new EMAIndicator(macd, 18);
+        
+        // Entry rule
+        Rule entryRule = new OverIndicatorRule(shortEma, longEma) // Trend
+                .and(new CrossedDownIndicatorRule(stochasticOscillK, 20)) // Signal 1
+                .and(new OverIndicatorRule(macd, emaMacd)); // Signal 2
+        
+        // Exit rule
+        Rule exitRule = new UnderIndicatorRule(shortEma, longEma) // Trend
+                .and(new CrossedUpIndicatorRule(stochasticOscillK,20)) // Signal 1
+                .and(new UnderIndicatorRule(macd, emaMacd)); // Signal 2
+        
+        return new BaseStrategy(entryRule, exitRule);
+    }
+
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        List<CurrencyKlineDTO> currencyKlineDTOS = spotProductAPIService.getCandles("okex",  "btc-usdt",60,null, null);
+        // Getting the time series
+        if (timeSeries == null) {
+            timeSeries = new BaseTimeSeries();
+
+            for (int i = currencyKlineDTOS.size() -1; i>=0;i-- ) {
+                CurrencyKlineDTO currencyKlineDTO = currencyKlineDTOS.get(i);
+                ZonedDateTime beginTime = ZonedDateTime.ofInstant(
+                        Instant.ofEpochMilli(Long.parseLong(currencyKlineDTO.getTime()) * 1000), ZoneId.systemDefault());
+
+                timeSeries.addBar(beginTime, currencyKlineDTO.getOpen(), currencyKlineDTO.getHigh(), currencyKlineDTO.getLow(), currencyKlineDTO.getClose());
+                if (timeSeries.getBarCount() > 26) {
+                    break;
+                }
+            }
+            // Building the trading strategy
+            strategy = buildStrategy(timeSeries);
+
+            // Initializing the trading history
+            tradingRecord = new BaseTradingRecord();
+
+        } else {
+            CurrencyKlineDTO currencyKlineDTO = currencyKlineDTOS.get(currencyKlineDTOS.size() - 1);
+
+            ZonedDateTime beginTime = ZonedDateTime.ofInstant(
+            Instant.ofEpochMilli(Long.parseLong(currencyKlineDTO.getTime()) * 1000), ZoneId.systemDefault());
+            timeSeries.addBar(beginTime, currencyKlineDTO.getOpen(), currencyKlineDTO.getHigh(), currencyKlineDTO.getLow(), currencyKlineDTO.getClose());
+
+        }
+
+        log.info("Current bar is {}", JSON.toJSONString(timeSeries.getBarData()));
+
+
+        int endIndex = timeSeries.getEndIndex();
+        Bar newBar = timeSeries.getLastBar();
+        if (strategy.shouldEnter(endIndex)) {
+            // Our strategy should enter
+            log.info("Strategy should ENTER on " + endIndex);
+            boolean entered = tradingRecord.enter(endIndex, newBar.getClosePrice(), PrecisionNum.valueOf(10));
+            if (entered) {
+                Order entry = tradingRecord.getLastEntry();
+                log.info("Entered on " + entry.getIndex()
+                        + " (price=" + entry.getPrice().doubleValue()
+                        + ", amount=" + entry.getAmount().doubleValue() + ")");
+            }
+        } else if (strategy.shouldExit(endIndex)) {
+            // Our strategy should exit
+            log.info("Strategy should EXIT on " + endIndex);
+            boolean exited = tradingRecord.exit(endIndex, newBar.getClosePrice(), PrecisionNum.valueOf(10));
+            if (exited) {
+                Order exit = tradingRecord.getLastExit();
+                log.info("Exited on " + exit.getIndex()
+                        + " (price=" + exit.getPrice().doubleValue()
+                        + ", amount=" + exit.getAmount().doubleValue() + ")");
+            }
+        }
+    }
+}
