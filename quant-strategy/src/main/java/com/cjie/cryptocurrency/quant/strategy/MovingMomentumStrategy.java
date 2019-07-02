59d4ddde8c6f1db4f4c438d808feb9f730c239ae
cjie888 <cjie888@163.com>
2019-07-02 13:09:45 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MovingMomentumStrategy.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MovingMomentumStrategy.java
@@ -33,7 +33,7 @@ import java.util.List;
  * @see <a href="http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum">
  *     http://stockcharts.com/help/doku.php?id=chart_school:trading_strategies:moving_momentum</a>
  */
-@ElasticJobConf(name = "movingMomentumJob", cron = "10 */1 * * * ?",
+@ElasticJobConf(name = "movingMomentumJob", cron = "30 */1 * * * ?",
         description = "移动动量策略", eventTraceRdbDataSource = "logDatasource")
 @Slf4j
 public class MovingMomentumStrategy implements SimpleJob {
