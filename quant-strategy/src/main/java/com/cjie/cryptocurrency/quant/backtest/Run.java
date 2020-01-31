bee1c165102b8ba9631a7430f9e270c5f0c761ad
cjie888 <cjie888@163.com>
2020-01-31 13:18:00 +0800

    change taks

---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/backtest/Run.java
@@ -0,0 +1,33 @@
+package com.cjie.cryptocurrency.quant.backtest;
+
+
+import com.cjie.cryptocurrency.quant.backtest.strategies.SimpleRangeScalper;
+import org.ta4j.core.TimeSeries;
+
+import java.math.BigDecimal;
+
+/**
+ * Example how you could use and test strategies with example data of this repository
+ */
+public class Run {
+
+
+    public static void main(String[] args) {
+
+        // load data as TimeSeries
+        Loader loader = new Loader();
+        TimeSeries series = loader.getMinuteTimeSeries("/Users/hucj/cryptocurrency-quant/quant-strategy/src/main/data/btc_minutes.csv", "btc");
+
+        // create and initialize a strategy
+        SimpleRangeScalper simpleRangeScalper = new SimpleRangeScalper();
+        simpleRangeScalper.initStrategy(series);
+
+        // run strategy on time series and analyse results
+        StrategyAnalyser analyser = new StrategyAnalyser();
+        analyser.printAllResults(simpleRangeScalper);
+
+        // change parameters of the strategy and run again
+        simpleRangeScalper.setParams(20, BigDecimal.valueOf(0.5));
+        analyser.printAllResults(simpleRangeScalper);
+    }
+}
---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/backtest/strategies/SimpleRangeScalper.java
@@ -0,0 +1,137 @@
+package com.cjie.cryptocurrency.quant.backtest.strategies;
+
+import com.cjie.cryptocurrency.quant.backtest.StrategyBuilder;
+import org.ta4j.core.*;
+import org.ta4j.core.indicators.EMAIndicator;
+import org.ta4j.core.indicators.bollinger.BollingerBandsLowerIndicator;
+import org.ta4j.core.indicators.bollinger.BollingerBandsMiddleIndicator;
+import org.ta4j.core.indicators.bollinger.BollingerBandsUpperIndicator;
+import org.ta4j.core.indicators.helpers.*;
+import org.ta4j.core.indicators.statistics.StandardDeviationIndicator;
+import org.ta4j.core.num.Num;
+import org.ta4j.core.num.PrecisionNum;
+import org.ta4j.core.trading.rules.*;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+
+
+/*
+    http://www.investopedia.com/terms/s/scalping.asp
+    http://forexop.com/strategy/simple-range-scalper/
+ */
+public class SimpleRangeScalper implements StrategyBuilder {
+
+    private TimeSeries series;
+
+    private ClosePriceIndicator closePrice;
+    private Indicator<Num> maxPrice;
+    private Indicator<Num> minPrice;
+
+    private BollingerBandsUpperIndicator upperBollingerBand;
+    private BollingerBandsMiddleIndicator middleBollingerBand;
+    private BollingerBandsLowerIndicator lowerBollingeBand;
+
+    // parameters
+    private BigDecimal takeProfitValue;
+    private int emaForBollingerBandValue;
+    /**
+     * Constructor
+     */
+    public SimpleRangeScalper(){}
+
+    public SimpleRangeScalper(TimeSeries series){
+        initStrategy(series);
+    }
+
+    @Override
+    public void initStrategy(TimeSeries series) {
+        this.series = series;
+        this.minPrice = new MinPriceIndicator(this.series);
+        this.closePrice = new ClosePriceIndicator(this.series);
+        this.maxPrice = new MaxPriceIndicator(this.series);
+        setParams(14, BigDecimal.valueOf(0.01));
+    }
+
+    @Override
+    public Strategy buildStrategy(Order.OrderType type){
+        if (type.equals(Order.OrderType.SELL)) {
+            return getShortStrategy();
+        }
+        return getLongStrategy();
+    }
+
+    @Override
+    public TradingRecord getTradingRecord(Order.OrderType type) {
+        TimeSeriesManager seriesManager = new TimeSeriesManager(series);
+        return seriesManager.run(buildStrategy(type), type);
+    }
+
+    @Override
+    public TimeSeries getTimeSeries(){
+        return this.series;
+    }
+
+    @Override
+    public String getName(){
+        return "Simple Range Scalper";
+    }
+
+    @Override
+    public List<String> getParamters(){
+        ArrayList<String> parameters = new ArrayList<String>();
+        String takeProfit = "Take Profit: "+ this.takeProfitValue;
+        String ema = "EMA :"+ this.emaForBollingerBandValue;
+        parameters.add(takeProfit);
+        parameters.add(ema);
+        return  parameters;
+    }
+
+    /**
+     * call this function to change the parameter of the strategy
+     * @param emaForBollingerBandValue exponential moving average the bollinger bands are based on
+     * @param takeProfitValue close a trade if this percentage profit is reached
+     */
+    public void setParams(int emaForBollingerBandValue, BigDecimal takeProfitValue){
+        this.takeProfitValue = takeProfitValue;
+        this.emaForBollingerBandValue = emaForBollingerBandValue;
+
+        EMAIndicator ema = new EMAIndicator(this.closePrice, emaForBollingerBandValue);
+        StandardDeviationIndicator standardDeviation = new StandardDeviationIndicator(this.closePrice, emaForBollingerBandValue);
+        this.middleBollingerBand = new BollingerBandsMiddleIndicator(ema);
+        this.lowerBollingeBand = new BollingerBandsLowerIndicator(this.middleBollingerBand, standardDeviation);
+        this.upperBollingerBand = new BollingerBandsUpperIndicator(this.middleBollingerBand, standardDeviation);
+    }
+
+    private Strategy getLongStrategy() {
+
+        Indicator<Num> d_upper_middle = new DifferenceIndicator(this.upperBollingerBand, this.middleBollingerBand);
+        // exit if half way up to middle reached
+        Indicator<Num> threshold = new MultiplierIndicator(d_upper_middle, Double.valueOf(0.5));
+
+        Rule entrySignal = new CrossedUpIndicatorRule(this.maxPrice, this.upperBollingerBand);
+        Rule entrySignal2 = new UnderIndicatorRule(this.minPrice, this.upperBollingerBand);
+
+        Rule exitSignal = new CrossedDownIndicatorRule(this.closePrice, threshold);
+        Rule exitSignal2 = new StopGainRule(closePrice, this.takeProfitValue);
+
+        return new BaseStrategy(entrySignal.and(entrySignal2), exitSignal.or(exitSignal2), 5);
+
+    }
+
+    private Strategy getShortStrategy(){
+
+        Indicator<Num> d_middle_lower = new DifferenceIndicator(this.middleBollingerBand, this.lowerBollingeBand);
+        // exit if half way down to middle reached
+        Indicator<Num> threshold = new MultiplierIndicator(d_middle_lower, Double.valueOf(0.5));
+
+        Rule entrySignal = new CrossedDownIndicatorRule(this.minPrice, this.lowerBollingeBand);
+        Rule entrySignal2 = new OverIndicatorRule(this.maxPrice, this.lowerBollingeBand);
+
+        Rule exitSignal = new CrossedUpIndicatorRule(this.closePrice, threshold);
+        Rule exitSignal2 = new StopLossRule(closePrice, this.takeProfitValue); // stop loss long = stop gain short?
+
+        return new BaseStrategy(entrySignal.and(entrySignal2), exitSignal.or(exitSignal2), 5);
+    }
+}
