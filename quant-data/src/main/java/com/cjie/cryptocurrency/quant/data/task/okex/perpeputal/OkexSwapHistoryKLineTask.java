4323eec8706334102c5c8bbfaafc8f4022a1ebff
cjie888 <cjie888@163.com>
2020-02-04 11:07:07 +0800

    change taks

--- quant-data/src/main/java/com/cjie/cryptocurrency/quant/data/task/okex/perpeputal/OkexSwapHistoryKLineTask.java
+++ quant-data/src/main/java/com/cjie/cryptocurrency/quant/data/task/okex/perpeputal/OkexSwapHistoryKLineTask.java
@@ -14,6 +14,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 @Component
@@ -118,8 +119,10 @@ public class OkexSwapHistoryKLineTask {
                         continue;
                     }
                     int count = 0;
+                    String endTime =  dateFormat.format(new Date(minKline.getKlineTime().getTime() + 60L * 1000 * 10 * getMintutes(type)));
                     String klineS = swapMarketAPIService.getCandlesApi(instrumentId, null,
-                            dateFormat.format(minKline.getKlineTime().getTime() + 60L * 1000 * 10 * getMintutes(type)), String.valueOf(getGranularity(type)));
+                           endTime, String.valueOf(getGranularity(type)));
+
                     List<String[]> apiKlineVOs = JSON.parseObject(klineS, new TypeReference<List<String[]>>() {
                     });
                     for (String[] apiKlineVO : apiKlineVOs) {
