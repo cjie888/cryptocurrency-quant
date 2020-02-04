644f16a3e5f3320138ff530d3e624b622ee90399
cjie888 <cjie888@163.com>
2020-02-04 13:05:06 +0800

    change taks

--- start_data.sh
+++ start_data.sh
@@ -28,6 +28,6 @@ else
     echo "kill $input1 fail"
 fi
 
-nohup java -jar quant-data-1.0.0-SNAPSHOT.jar &
+nohup java -jar quant-data-1.0.0-SNAPSHOT.jar >/dev/null 2>&1 &
 
 echo "------success---------"
