2c2ad088074be37e9626ce8f6fe83ce6c04bc464
cjie888 <cjie888@163.com>
2020-02-06 10:56:28 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/BaseSwapStrategyJob.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/BaseSwapStrategyJob.java
@@ -118,7 +118,8 @@ public abstract class BaseSwapStrategyJob  {
             int endIndex = timeSeries.getEndIndex();
             Bar newBar = timeSeries.getLastBar();
 
-            log.info("kline" + JSON.toJSONString(timeSeries.getBarData()));
+            log.info("kline date:" + JSON.toJSONString(newBar.getBeginTime()) + "price:" + newBar.getClosePrice());
+
 
             if ((longTradingRecord.getCurrentTrade().isNew() || longTradingRecord.getCurrentTrade().isClosed() )&&longStrategy.shouldEnter(endIndex, longTradingRecord)) {
                 StringBuilder stringBuilder = new StringBuilder();
