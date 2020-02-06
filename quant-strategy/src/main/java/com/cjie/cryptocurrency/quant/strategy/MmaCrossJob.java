581b9d6bd3020dc3a405d9e0a66affc291159ae7
cjie888 <cjie888@163.com>
2020-02-06 11:51:43 +0800

    change taks

---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MmaCrossJob.java
@@ -0,0 +1,40 @@
+package com.cjie.cryptocurrency.quant.strategy;
+
+
+import com.cjie.cryptocurrency.quant.backtest.StrategyBuilder;
+import com.cxytiandi.elasticjob.annotation.ElasticJobConf;
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import lombok.extern.slf4j.Slf4j;
+
+import org.ta4j.core.*;
+
+
+import java.math.BigDecimal;
+
+@ElasticJobConf(name = "mmaCrossJob", cron = "10 */1 * * * ?",
+        description = "mmaCross", eventTraceRdbDataSource = "logDatasource")
+@Slf4j(topic = "strategy")
+public class MmaCrossJob extends BaseSwapStrategyJob implements SimpleJob {
+
+
+    @Override
+    public void execute(ShardingContext shardingContext) {
+        log.info("start mma cross job");
+        executeStrategy("BTC-USD-SWAP");
+        executeStrategy("ETH-USD-SWAP");
+        executeStrategy("EOS-USD-SWAP");
+        executeStrategy("LTC-USD-SWAP");
+        executeStrategy("XRP-USD-SWAP");
+        executeStrategy("BCH-USD-SWAP");
+        executeStrategy("BSV-USD-SWAP");
+        executeStrategy("ETC-USD-SWAP");
+    }
+
+
+    @Override
+    public StrategyBuilder buildStrategy(TimeSeries timeSeries) {
+        MmaCrossStrategy strategy = new MmaCrossStrategy(timeSeries);
+        return strategy;
+    }
+}
