e9c55d46f93abf19cb647846f53783b178a5c1f2
cjie888 <cjie888@163.com>
2020-02-28 20:57:17 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapTask.java
@@ -15,20 +15,21 @@ public class SwapTask {
 
     @Scheduled(cron = "13 */1 * * * ?")
     public  void netGrid() {
-       // swapService.netGrid("BTC-USD-SWAP", "1", 70.0, 0.002);
-        swapService.transfer("BTC-USD-SWAP",  0.002);
+        swapService.netGrid("BTC-USD-SWAP", "1", 80.0, 0.002);
+        //swapService.transfer("BTC-USD-SWAP",  0.002);
         swapService.netGrid("ETH-USD-SWAP", "1", 1.2,0.1);
        // swapService.transfer("ETH-USD-SWAP",0.1);
 //        swapService.netGrid("EOS-USD-SWAP", "2", 0.018, 1.0);
-//        swapService.netGrid("BCH-USD-SWAP", "2", 0.8, 0.1);
-        swapService.transfer("BCH-USD-SWAP", 0.1);
+        swapService.netGrid("BCH-USD-SWAP", "1", 1.0, 0.1);
+       // swapService.transfer("BCH-USD-SWAP", 0.1);
     //    swapService.netGrid("XRP-USD-SWAP", "2", 0.0015,20.0);
         swapService.transfer("XRP-USD-SWAP", 20.0);
 //        swapService.netGrid("LTC-USD-SWAP", "2", 0.25, 0.3);
         swapService.transfer("LTC-USD-SWAP", 0.3);
 //        swapService.netGrid("ETH-USDT-SWAP", "140", 0.6, 5.0);
-        swapService.transfer("BTC-USDT-SWAP", 10.0);
-        swapService.transfer("BCH-USDT-SWAP", 10.0);
+        swapService.netGrid("BTC-USDT-SWAP", "0.002", 80.0, 10.0);
+        //swapService.transfer("BTC-USDT-SWAP", 10.0);
+        //swapService.transfer("BCH-USDT-SWAP", 10.0);
 
 
     }
