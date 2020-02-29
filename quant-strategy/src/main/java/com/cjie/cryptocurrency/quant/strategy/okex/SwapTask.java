d6ac22af47edaa8bf51e82a10c0822969cb4da83
cjie888 <cjie888@163.com>
2020-02-29 20:49:05 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapTask.java
@@ -27,7 +27,7 @@ public class SwapTask {
         swapService.netGrid("LTC-USD-SWAP", "1", 0.3, 0.3, 130);
      //   swapService.transfer("LTC-USD-SWAP", 0.3);
 //        swapService.netGrid("ETH-USDT-SWAP", "140", 0.6, 5.0);
-        swapService.netGrid("BTC-USDT-SWAP", "6", 60.0, 10.0, 900);
+        swapService.netGrid("BTC-USDT-SWAP", "10", 60.0, 10.0, 900);
         //swapService.transfer("BTC-USDT-SWAP", 10.0);
         //swapService.transfer("BCH-USDT-SWAP", 10.0);
         swapService.netGrid("BCH-USDT-SWAP", "1", 1.5, 10.0, 27);
