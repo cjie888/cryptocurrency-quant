a0f23709559f6e1626c6f7c1049bd653d0b99477
cjie888 <cjie888@163.com>
2020-03-01 11:33:27 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
@@ -235,12 +235,14 @@ public class SwapService {
                         weiXinMessageService.sendMessage("划转" + currency.toUpperCase(), "划转" + instrumentId + ", 数量：" + transferAmount);
                     } catch (Exception e) {
                         log.error("transfer {} {} from financial to swap error", instrumentId, transferAmount, e);
-                        transferIn.setFrom(5);
-                        transferIn.setInstrument_id(currency + "_usdt");
-                        JSONObject result = accountAPIService.transfer("okex", transferIn);
+
                         try {
+                            Thread.sleep(2000);
+                            transferIn.setFrom(5);
+                            transferIn.setInstrument_id(currency + "_usdt");
+                            JSONObject result = accountAPIService.transfer("okex", transferIn);
                             log.info("transfer {} {} from spot margin to swap", transferAmount, JSON.toJSONString(result));
-                            weiXinMessageService.sendMessage("划转" + currency.toUpperCase(), "划转" + instrumentId + ", 数量：" + transferAmount);
+                            weiXinMessageService.sendMessage("划转" + currency.toUpperCase() + "币币杠杆", "划转" + instrumentId + ", 数量：" + transferAmount);
                         } catch (Exception e1) {
                             log.error("transfer {} {} from spot margin to swap error", instrumentId, transferAmount, e1);
 
