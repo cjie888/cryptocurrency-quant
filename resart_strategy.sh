5d258eb3db83d61f49c8b4105893684d2cb51070
cjie888 <cjie888@163.com>
2020-03-06 17:12:55 +0800

    change taks

--- resart_strategy.sh
+++ resart_strategy.sh
@@ -2,6 +2,8 @@
 tail -n100 /var/log/mine/info.log|grep "当前价格"  > /dev/null
 if [ $? -eq 0 ]; then
     echo "Found!"
+    cd /root/project/cryptocurrency-quant
+    sh start_strategy.sh > /dev/null
 else
     echo "Not found!"
 fi
