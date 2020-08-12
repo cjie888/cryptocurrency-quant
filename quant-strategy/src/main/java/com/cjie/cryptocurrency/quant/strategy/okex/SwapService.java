3f1818aa5902a9d173ddb4c0739c90b7bf449a2f
cjie888 <cjie888@163.com>
2020-08-12 11:10:55 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
@@ -72,9 +72,9 @@ public class SwapService {
         //costs.put("EOS-USD-SWAP",new BigDecimal("60"));
         costs.put("XRP-USD-SWAP",new BigDecimal("900"));
         costs.put("LTC-USD-SWAP",new BigDecimal("3"));
-        costs.put("BTC-USDT-SWAP", new BigDecimal("300"));
+        costs.put("BTC-USDT-SWAP", new BigDecimal("2000"));
         costs.put("BCH-USDT-SWAP", new BigDecimal("200"));
-        costs.put("ATOM-USDT-SWAP", new BigDecimal("100"));
+        costs.put("ATOM-USDT-SWAP", new BigDecimal("150"));
         //costs.put("ETH-USDT-SWAP", new BigDecimal("100"));
         String accounts = swapUserAPIServive.getAccounts();
         log.info("获取所有账户信息{}", JSON.toJSONString(accounts));
