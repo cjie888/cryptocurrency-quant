5c2d5703a4a77b327792745fda647afc4f902593
cjie888 <cjie888@163.com>
2020-11-23 11:32:16 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SwapService.java
@@ -251,17 +251,17 @@ public class SwapService {
                     } catch (Exception e) {
                         log.error("transfer {} {} from financial to swap error", instrumentId, transferAmount, e);
 
-                        try {
-                            Thread.sleep(2000);
-                            transferIn.setFrom(5);
-                            transferIn.setInstrument_id(currency + "-usdt");
-                            JSONObject result = accountAPIService.transfer("okexsub1", transferIn);
-                            log.info("transfer {} {} from spot margin to swap", transferAmount, JSON.toJSONString(result));
-                           // weiXinMessageService.sendMessage("划转" + currency.toUpperCase() + "币币杠杆", "划转" + instrumentId + ", 数量：" + transferAmount);
-                        } catch (Exception e1) {
-                            log.error("transfer {} {} from spot margin to swap error", instrumentId, transferAmount, e1);
-
-                        }
+//                        try {
+//                            Thread.sleep(2000);
+//                            transferIn.setFrom(5);
+//                            transferIn.setInstrument_id(currency + "-usdt");
+//                            JSONObject result = accountAPIService.transfer("okexsub1", transferIn);
+//                            log.info("transfer {} {} from spot margin to swap", transferAmount, JSON.toJSONString(result));
+//                           // weiXinMessageService.sendMessage("划转" + currency.toUpperCase() + "币币杠杆", "划转" + instrumentId + ", 数量：" + transferAmount);
+//                        } catch (Exception e1) {
+//                            log.error("transfer {} {} from spot margin to swap error", instrumentId, transferAmount, e1);
+//
+//                        }
                     }
                 }
                 if (Double.valueOf(apiAccountVO.getMargin_ratio()) < 0.10) {
