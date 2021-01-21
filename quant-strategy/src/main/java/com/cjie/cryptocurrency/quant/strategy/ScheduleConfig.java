51048d7ef711e90701588e0667026c1b0e0bea5f
cjie888 <cjie888@163.com>
2021-01-21 22:37:24 +0800

    change taks

---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/ScheduleConfig.java
@@ -0,0 +1,21 @@
+package com.cjie.cryptocurrency.quant.strategy;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+import java.util.concurrent.Executors;
+
+/**
+ * 多线程执行定时任务
+
+ */
+@Configuration
+//所有的定时任务都放在一个线程池中，定时任务启动时使用不同都线程。
+public class ScheduleConfig implements SchedulingConfigurer {
+    @Override
+    public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+        //设定一个长度10的定时任务线程池
+        taskRegistrar.setScheduler(Executors.newScheduledThreadPool(5));
+    }
+}
