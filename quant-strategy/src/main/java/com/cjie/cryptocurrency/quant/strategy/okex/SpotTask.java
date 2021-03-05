69b8eb7953fad7641f7a253149a896c103a15aaa
cjie888 <cjie888@163.com>
2021-03-05 14:13:21 +0800

    change taks

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotTask.java
@@ -75,7 +75,7 @@ public class SpotTask {
 
     }
 
-    @Scheduled(cron = "7 3 */2 * * ?")
+    @Scheduled(cron = "7 17 */2 * * ?")
     public  void benefit2() {
         LocalDateTime now = LocalDateTime.now();
         LocalDateTime startTime = now.withDayOfMonth(1).withHour(0).withMinute(0).withSecond(0).withNano(0);
