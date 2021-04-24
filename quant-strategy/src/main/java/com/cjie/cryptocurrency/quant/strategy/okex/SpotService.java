67d2af3fa3a7e5354131b0c2de3d3d7b15cca057
hucj <julius.hu@huolala.cn>
2021-04-24 14:11:06 +0800

    change config

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotService.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotService.java
@@ -449,11 +449,13 @@ public class SpotService {
                     profit = amount.multiply(sellSumSymbol.subtract(buySumSymbol)).setScale(4, RoundingMode.DOWN);
                     allProfit = allProfit.add(profit);
                 }
-                stringBuilder.append(symbol + ":买入(次数:" + buyCountSymbol + ",数量:" + buyAmountSymbol.setScale(3, RoundingMode.DOWN).stripTrailingZeros()
-                        + ",价格:" + buySumSymbol.setScale(4, RoundingMode.DOWN).stripTrailingZeros() + ")，卖出(次数:" + sellCountSymbol
-                        + ",数量:" + sellAmountSymbol.setScale(3, RoundingMode.DOWN).stripTrailingZeros()
-                        + ",价格:" + sellSumSymbol.setScale(4, RoundingMode.DOWN).stripTrailingZeros()
-                        + "),收益:" + profit.setScale(2, RoundingMode.DOWN).stripTrailingZeros() +  "\r\n\r\n");
+                stringBuilder.append(symbol + ":买入(次数:" + buyCountSymbol
+                        + ",数量:" + buyAmountSymbol.setScale(3, RoundingMode.DOWN).stripTrailingZeros().toPlainString()
+                        + ",价格:" + buySumSymbol.setScale(4, RoundingMode.DOWN).stripTrailingZeros().toPlainString()
+                        + ")，卖出(次数:" + sellCountSymbol
+                        + ",数量:" + sellAmountSymbol.setScale(3, RoundingMode.DOWN).stripTrailingZeros().toPlainString()
+                        + ",价格:" + sellSumSymbol.setScale(4, RoundingMode.DOWN).stripTrailingZeros().toPlainString()
+                        + "),收益:" + profit.setScale(2, RoundingMode.DOWN).stripTrailingZeros().toPlainString() +  "\r\n\r\n");
 
             }
             String message = MessageFormat.format("买入次数:{0},卖出次数:{1},总收益:{2}\r\n\r\n", buyCount, sellCount, allProfit.setScale(2, RoundingMode.DOWN).toPlainString());
