88913552efa16f660887b7da78d3d8619bd84166
cjie888 <cjie888@163.com>
2021-12-19 12:48:33 +0800

    api v5

--- quant-base/src/main/java/com/cjie/cryptocurrency/quant/api/okex/v5/bean/account/result/AccountDetail.java
+++ quant-base/src/main/java/com/cjie/cryptocurrency/quant/api/okex/v5/bean/account/result/AccountDetail.java
@@ -4,11 +4,52 @@ public class AccountDetail {
 
     private String availBal;
 
+    private String ccy;
+
+
+    private String eq;   // 币种总权益
+
+    private String availEq;
+
+    private String cashBal; //币种余额
+
     public String getAvailBal() {
         return availBal;
     }
 
     public void setAvailBal(String availBal) {
         this.availBal = availBal;
+    }
+
+    public String getCcy() {
+        return ccy;
+    }
+
+    public void setCcy(String ccy) {
+        this.ccy = ccy;
+    }
+
+    public String getEq() {
+        return eq;
+    }
+
+    public void setEq(String eq) {
+        this.eq = eq;
+    }
+
+    public String getAvailEq() {
+        return availEq;
+    }
+
+    public void setAvailEq(String availEq) {
+        this.availEq = availEq;
+    }
+
+    public String getCashBal() {
+        return cashBal;
+    }
+
+    public void setCashBal(String cashBal) {
+        this.cashBal = cashBal;
     }
 }
