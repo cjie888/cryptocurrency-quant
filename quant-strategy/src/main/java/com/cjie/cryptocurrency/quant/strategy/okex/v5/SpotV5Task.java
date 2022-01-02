daf23eddd3b35a7c0ecac65c0b2be09b9c6f8c30
cjie888 <cjie888@163.com>
2022-01-02 18:45:42 +0800

    api v5

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
@@ -14,7 +14,7 @@ public class SpotV5Task {
     @Autowired
     private SpotV5Service spotService;
 
-    @Scheduled(cron = "7/13 * * * * ?")
+    @Scheduled(cron = "1/29 * * * * ?")
     public  void netGrid() {
 
         spotService.netGrid("okexsub2", "MANA-USDT", "1", 0.03);
@@ -26,16 +26,66 @@ public class SpotV5Task {
         spotService.netGrid("okexsub1", "ETC-USDT", "1", 0.03);
         spotService.netGrid("okexsub1", "BSV-USDT", "0.05000000", 0.03);
 
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        spotService.netGrid("okex", "EOS-USDT", "2", 0.03);
+        spotService.netGrid("okex", "OKT-USDT", "1", 0.03);
+        spotService.netGrid("okex", "DOGE-USDT", "1000", 0.03);
+        spotService.netGrid("okex", "UNI-USDT", "1", 0.03);
+        spotService.netGrid("okex", "XLM-USDT", "50", 0.03);
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        spotService.netGrid("okex", "IOTA-USDT", "30", 0.03);
+        spotService.netGrid("okex", "XTZ-USDT", "10", 0.03);
+        spotService.netGrid("okex", "AAVE-USDT", "0.1", 0.03);
+        spotService.netGrid("okex", "AVAX-USDT", "1", 0.03);
+        spotService.netGrid("okex", "ICX-USDT", "10", 0.03);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        spotService.netGrid("okex", "THETA-USDT", "5", 0.03);
+
 
 //ltc 6  xrp 3 etc 5
     }
 
 
-    @Scheduled(cron = "3/11 * * * * ?")
+    @Scheduled(cron = "11/23 * * * * ?")
     public  void netGrid2() {
         spotService.netGrid("okexsub1", "SUSHI-USDT", "1", 0.03);
         spotService.netGrid("okexsub1", "ATOM-USDT", "1", 0.03);
         spotService.netGrid("okexsub1", "LINK-USDT", "0.50000000", 0.03);
         spotService.netGrid("okexsub1", "DOT-USDT", "1", 0.03);
+
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        spotService.netGrid("okex", "BTC-USDT", "0.00100000", 0.03);
+        spotService.netGrid("okex", "OKB-USDT", "10", 0.03);
+        spotService.netGrid("okex", "XMR-USDT", "0.1", 0.03);
+        spotService.netGrid("okex", "ZEC-USDT", "0.2", 0.03);
+        spotService.netGrid("okex", "FIL-USDT", "1", 0.03);
+        spotService.netGrid("okex", "ZIL-USDT", "100", 0.03);
+        try {
+            Thread.sleep(1000);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        spotService.netGrid("okex", "ADA-USDT", "20", 0.03);
+        spotService.netGrid("okex", "XEM-USDT", "30", 0.03);
+        spotService.netGrid("okex", "DASH-USDT", "0.1", 0.03);
+        spotService.netGrid("okex", "GRT-USDT", "10", 0.03);
+        spotService.netGrid("okex", "COMP-USDT", "0.05", 0.03);
     }
 }
