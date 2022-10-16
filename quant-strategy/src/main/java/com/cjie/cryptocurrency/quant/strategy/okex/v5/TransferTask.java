b3bce71a27839a46a5e70d787bfa5773d11e0fa8
cjie888 <cjie888@163.com>
2022-10-16 19:41:05 +0800

    change ratio

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
@@ -18,43 +18,43 @@ public class TransferTask {
     @Scheduled(cron = "29 50 */1 * * ?")
     public void transfer() {
 
-        transferService.transfer("okexsub2", "MANA", "1", 1.0);
-        transferService.transfer("okex", "DOGE", "1000", 1.0);
-        transferService.transfer("okex", "OKB", "10", 1.0);
-        transferService.transfer("okexsub1", "ETH", "0.05", 1.0);
-        transferService.transfer("okex", "AVAX", "1", 1.0);
-        transferService.transfer("okex", "BTC", "0.001", 1.0);
-        transferService.transfer("okexsub1", "ETC", "1", 1.0);
-        transferService.transfer("okexsub1", "ATOM", "1", 1.0);
-        transferService.transfer("okex", "XTZ", "10", 1.0);
-        transferService.transfer("okex", "IOTA", "30", 1.0);
-        transferService.transfer("okex", "ZEC", "0.2", 1.0);
-        transferService.transfer("okex", "FIL", "1", 1.0);
-        transferService.transfer("okexsub1", "DOT", "1", 1.0);
-        transferService.transfer("okexsub1", "BCH", "0.04", 1.0);
+        transferService.transfer("okexsub2", "MANA", "1", 3.0);
+        transferService.transfer("okex", "DOGE", "1000", 3.0);
+        transferService.transfer("okex", "OKB", "10", 3.0);
+        transferService.transfer("okexsub1", "ETH", "0.05", 3.0);
+        transferService.transfer("okex", "AVAX", "1", 3.0);
+        transferService.transfer("okex", "BTC", "0.001", 3.0);
+        transferService.transfer("okexsub1", "ETC", "1", 3.0);
+        transferService.transfer("okexsub1", "ATOM", "1", 3.0);
+        transferService.transfer("okex", "XTZ", "10", 3.0);
+        transferService.transfer("okex", "IOTA", "30", 3.0);
+        transferService.transfer("okex", "ZEC", "0.2", 3.0);
+        transferService.transfer("okex", "FIL", "1", 3.0);
+        transferService.transfer("okexsub1", "DOT", "1", 3.0);
+        transferService.transfer("okexsub1", "BCH", "0.04", 3.0);
 
-        transferService.transfer("okex", "ADA", "20", 1.0);
-        transferService.transfer("okex", "AAVE", "0.1", 1.0);
-        transferService.transfer("okex", "THETA", "5", 1.0);
-        transferService.transfer("okex", "XMR", "0.1", 1.0);
+        transferService.transfer("okex", "ADA", "20", 3.0);
+        transferService.transfer("okex", "AAVE", "0.1", 3.0);
+        transferService.transfer("okex", "THETA", "5", 3.0);
+        transferService.transfer("okex", "XMR", "0.1", 3.0);
 
-        transferService.transfer("okexsub1", "XRP", "20", 1.0);
-        transferService.transfer("okexsub1", "LTC", "0.1", 1.0);
+        transferService.transfer("okexsub1", "XRP", "20", 3.0);
+        transferService.transfer("okexsub1", "LTC", "0.1", 3.0);
 
-        transferService.transfer("okex", "UNI", "1", 1.0);
-        transferService.transfer("okex", "DASH", "0.1", 1.0);
-        transferService.transfer("okex", "XLM", "50", 1.0);
+        transferService.transfer("okex", "UNI", "1", 3.0);
+        transferService.transfer("okex", "DASH", "0.1", 3.0);
+        transferService.transfer("okex", "XLM", "50", 3.0);
         transferService.transfer("okex", "COMP", "0.05", 3.0);
 
-        transferService.transfer("okexsub1", "LINK", "0.5", 1.0);
-        transferService.transfer("okexsub1", "SUSHI", "1", 1.0);
+        transferService.transfer("okexsub1", "LINK", "0.5", 3.0);
+        transferService.transfer("okexsub1", "SUSHI", "1", 3.0);
 
-        transferService.transfer("okex", "ZIL", "100", 1.0);
-        transferService.transfer("okex", "GRT", "10", 1.0);
-        transferService.transfer("okex", "EOS", "2", 1.0);
-        transferService.transfer("okex", "XEM", "30", 1.0);
+        transferService.transfer("okex", "ZIL", "100", 3.0);
+        transferService.transfer("okex", "GRT", "10", 3.0);
+        transferService.transfer("okex", "EOS", "2", 3.0);
+        transferService.transfer("okex", "XEM", "30", 3.0);
 
-        transferService.transfer("okexsub1", "BSV", "0.05", 1.0);
+        transferService.transfer("okexsub1", "BSV", "0.05", 3.0);
 
     }
 }
