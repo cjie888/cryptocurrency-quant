4ab9e04eb82039ac3db1cb20c95d023d45e9c970
胡成杰 <cjie888@163.com>
2023-07-16 20:49:41 +0800

    change config

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
@@ -16,24 +16,24 @@ public class SpotV5Task {
     public  void netGrid() {
 
         spotService.netGrid("okexsub2", "MANA-USDT", "1", 0.03);
-        spotService.netGrid("okexsub1", "ETH-USDT", "0.05000000", 0.03);
-        spotService.netGrid("okexsub1", "BCH-USDT", "0.04000000", 0.03);
-        spotService.netGrid("okexsub1", "LTC-USDT", "0.1", 0.03);
-        spotService.netGrid("okexsub1", "XRP-USDT", "20", 0.03);
-        spotService.netGrid("okexsub1", "ETC-USDT", "1", 0.03);
-        spotService.netGrid("okexsub1", "BSV-USDT", "0.05000000", 0.03);
+//        spotService.netGrid("okexsub1", "ETH-USDT", "0.05000000", 0.03);
+//        spotService.netGrid("okexsub1", "BCH-USDT", "0.04000000", 0.03);
+//        spotService.netGrid("okexsub1", "LTC-USDT", "0.1", 0.03);
+//        spotService.netGrid("okexsub1", "XRP-USDT", "20", 0.03);
+//        spotService.netGrid("okexsub1", "ETC-USDT", "1", 0.03);
+//        spotService.netGrid("okexsub1", "BSV-USDT", "0.05000000", 0.03);
 
         try {
             Thread.sleep(800);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        spotService.netGrid("okex", "EOS-USDT", "2", 0.03);
-        spotService.netGrid("okex", "OKT-USDT", "1", 0.03);
-        spotService.netGrid("okex", "DOGE-USDT", "1000", 0.03);
-        spotService.netGrid("okex", "UNI-USDT", "1", 0.03);
-        spotService.netGrid("okex", "XLM-USDT", "50", 0.03);
-        spotService.netGrid("okex", "DYDX-USDT", "5", 0.03);
+//        spotService.netGrid("okex", "EOS-USDT", "2", 0.03);
+//        spotService.netGrid("okex", "OKT-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "DOGE-USDT", "1000", 0.03);
+//        spotService.netGrid("okex", "UNI-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "XLM-USDT", "50", 0.03);
+//        spotService.netGrid("okex", "DYDX-USDT", "5", 0.03);
 
 
         try {
@@ -41,24 +41,23 @@ public class SpotV5Task {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        spotService.netGrid("okex", "IOTA-USDT", "30", 0.03);
-        spotService.netGrid("okex", "XTZ-USDT", "10", 0.03);
-        spotService.netGrid("okex", "AAVE-USDT", "0.1", 0.03);
-        spotService.netGrid("okex", "AVAX-USDT", "1", 0.03);
-        spotService.netGrid("okex", "ICX-USDT", "10", 0.03);
-        spotService.netGrid("okex", "OP-USDT", "5", 0.03);
+//        spotService.netGrid("okex", "IOTA-USDT", "30", 0.03);
+//        spotService.netGrid("okex", "XTZ-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "AAVE-USDT", "0.1", 0.03);
+//        spotService.netGrid("okex", "AVAX-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "ICX-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "OP-USDT", "5", 0.03);
         try {
             Thread.sleep(800);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        spotService.netGrid("okex", "THETA-USDT", "5", 0.03);
-        spotService.netGrid("okex", "APT-USDT", "1", 0.03);
-        spotService.netGrid("okex", "BNB-USDT", "0.05", 0.03);
-        //spotService.netGrid("okex", "LEO-USDT", "5", 0.03);
-        spotService.netGrid("okex", "TON-USDT", "10", 0.03);
-        spotService.netGrid("okex", "FTM-USDT", "10", 0.03);
-        spotService.netGrid("okex", "ARB-USDT", "20", 0.03);
+//        spotService.netGrid("okex", "THETA-USDT", "5", 0.03);
+//        spotService.netGrid("okex", "APT-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "BNB-USDT", "0.05", 0.03);
+//        spotService.netGrid("okex", "TON-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "FTM-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "ARB-USDT", "20", 0.03);
 
 
 
@@ -68,13 +67,13 @@ public class SpotV5Task {
 
     @Scheduled(cron = "11/23 * * * * ?")
     public  void netGrid2() {
-        spotService.netGrid("okex", "SUI-USDT", "10", 0.03);
-        spotService.netGrid("okexsub1", "SUSHI-USDT", "1", 0.03);
-        spotService.netGrid("okexsub1", "ATOM-USDT", "1", 0.03);
-        spotService.netGrid("okexsub1", "LINK-USDT", "0.50000000", 0.03);
-        spotService.netGrid("okexsub1", "DOT-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "SUI-USDT", "10", 0.03);
+//        spotService.netGrid("okexsub1", "SUSHI-USDT", "1", 0.03);
+//        spotService.netGrid("okexsub1", "ATOM-USDT", "1", 0.03);
+//        spotService.netGrid("okexsub1", "LINK-USDT", "0.50000000", 0.03);
+//        spotService.netGrid("okexsub1", "DOT-USDT", "1", 0.03);
         spotService.netGrid("okexsub2", "SOL-USDT", "1", 0.03);
-        spotService.netGrid("okex", "BLUR-USDT", "20", 0.03);
+//        spotService.netGrid("okex", "BLUR-USDT", "20", 0.03);
 
 
         try {
@@ -82,22 +81,22 @@ public class SpotV5Task {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        spotService.netGrid("okex", "BTC-USDT", "0.00100000", 0.03);
-        //spotService.netGrid("okex", "OKB-USDT", "10", 0.03);
-        spotService.netGrid("okex", "XMR-USDT", "0.1", 0.03);
-        spotService.netGrid("okex", "ZEC-USDT", "0.2", 0.03);
-        spotService.netGrid("okex", "FIL-USDT", "1", 0.03);
-        spotService.netGrid("okex", "ZIL-USDT", "100", 0.03);
+//        spotService.netGrid("okex", "BTC-USDT", "0.00100000", 0.03);
+//        //spotService.netGrid("okex", "OKB-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "XMR-USDT", "0.1", 0.03);
+//        spotService.netGrid("okex", "ZEC-USDT", "0.2", 0.03);
+//        spotService.netGrid("okex", "FIL-USDT", "1", 0.03);
+//        spotService.netGrid("okex", "ZIL-USDT", "100", 0.03);
         try {
             Thread.sleep(1000);
         } catch (Exception e) {
             e.printStackTrace();
         }
-        spotService.netGrid("okex", "ADA-USDT", "20", 0.03);
-        spotService.netGrid("okex", "XEM-USDT", "30", 0.03);
-        spotService.netGrid("okex", "DASH-USDT", "0.1", 0.03);
-        spotService.netGrid("okex", "GRT-USDT", "10", 0.03);
-        spotService.netGrid("okex", "COMP-USDT", "0.05", 0.03);
+//        spotService.netGrid("okex", "ADA-USDT", "20", 0.03);
+//        spotService.netGrid("okex", "XEM-USDT", "30", 0.03);
+//        spotService.netGrid("okex", "DASH-USDT", "0.1", 0.03);
+//        spotService.netGrid("okex", "GRT-USDT", "10", 0.03);
+//        spotService.netGrid("okex", "COMP-USDT", "0.05", 0.03);
         spotService.netGrid("okexsub2", "MATIC-USDT", "10", 0.03);
 
     }
--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
@@ -19,55 +19,55 @@ public class TransferTask {
     public void transfer() {
 
         transferService.transfer("okexsub2", "MANA", "1", 3.0);
-        transferService.transfer("okex", "DOGE", "1000", 3.0);
+//        transferService.transfer("okex", "DOGE", "1000", 3.0);
         //transferService.transfer("okex", "OKB", "10", 3.0);
-        transferService.transfer("okexsub1", "ETH", "0.05", 3.0);
-        transferService.transfer("okex", "AVAX", "1", 3.0);
-        transferService.transfer("okex", "BTC", "0.001", 3.0);
-        transferService.transfer("okexsub1", "ETC", "1", 3.0);
-        transferService.transfer("okexsub1", "ATOM", "1", 3.0);
-        transferService.transfer("okex", "XTZ", "10", 3.0);
-        transferService.transfer("okex", "IOTA", "30", 3.0);
-        transferService.transfer("okex", "ZEC", "0.2", 3.0);
-        transferService.transfer("okex", "FIL", "1", 3.0);
-        transferService.transfer("okexsub1", "DOT", "1", 3.0);
-        transferService.transfer("okexsub1", "BCH", "0.04", 3.0);
+//        transferService.transfer("okexsub1", "ETH", "0.05", 3.0);
+//        transferService.transfer("okex", "AVAX", "1", 3.0);
+//        transferService.transfer("okex", "BTC", "0.001", 3.0);
+//        transferService.transfer("okexsub1", "ETC", "1", 3.0);
+//        transferService.transfer("okexsub1", "ATOM", "1", 3.0);
+//        transferService.transfer("okex", "XTZ", "10", 3.0);
+//        transferService.transfer("okex", "IOTA", "30", 3.0);
+//        transferService.transfer("okex", "ZEC", "0.2", 3.0);
+//        transferService.transfer("okex", "FIL", "1", 3.0);
+//        transferService.transfer("okexsub1", "DOT", "1", 3.0);
+//        transferService.transfer("okexsub1", "BCH", "0.04", 3.0);
 
-        transferService.transfer("okex", "ADA", "20", 3.0);
-        transferService.transfer("okex", "AAVE", "0.1", 3.0);
-        transferService.transfer("okex", "THETA", "5", 3.0);
-        transferService.transfer("okex", "XMR", "0.1", 3.0);
+//        transferService.transfer("okex", "ADA", "20", 3.0);
+//        transferService.transfer("okex", "AAVE", "0.1", 3.0);
+//        transferService.transfer("okex", "THETA", "5", 3.0);
+//        transferService.transfer("okex", "XMR", "0.1", 3.0);
+//
+//        transferService.transfer("okexsub1", "XRP", "20", 3.0);
+//        transferService.transfer("okexsub1", "LTC", "0.1", 3.0);
 
-        transferService.transfer("okexsub1", "XRP", "20", 3.0);
-        transferService.transfer("okexsub1", "LTC", "0.1", 3.0);
-
-        transferService.transfer("okex", "UNI", "1", 3.0);
-        transferService.transfer("okex", "DASH", "0.1", 3.0);
-        transferService.transfer("okex", "XLM", "50", 3.0);
-        transferService.transfer("okex", "COMP", "0.05", 3.0);
-
-        transferService.transfer("okexsub1", "LINK", "0.5", 3.0);
-        transferService.transfer("okexsub1", "SUSHI", "1", 3.0);
-
-        transferService.transfer("okex", "ZIL", "100", 3.0);
-        transferService.transfer("okex", "GRT", "10", 3.0);
-        transferService.transfer("okex", "EOS", "2", 3.0);
-        transferService.transfer("okex", "XEM", "30", 3.0);
-
-        transferService.transfer("okexsub1", "BSV", "0.05", 3.0);
-        transferService.transfer("okex", "DYDX", "5", 3.0);
-        transferService.transfer("okex", "FTM", "10", 3.0);
-        transferService.transfer("okex", "OP", "5", 3.0);
-        transferService.transfer("okex", "APT", "1", 3.0);
-        transferService.transfer("okex", "BNB", "0.05", 3.0);
-        transferService.transfer("okex", "TON", "10", 3.0);
+//        transferService.transfer("okex", "UNI", "1", 3.0);
+//        transferService.transfer("okex", "DASH", "0.1", 3.0);
+//        transferService.transfer("okex", "XLM", "50", 3.0);
+//        transferService.transfer("okex", "COMP", "0.05", 3.0);
+//
+//        transferService.transfer("okexsub1", "LINK", "0.5", 3.0);
+//        transferService.transfer("okexsub1", "SUSHI", "1", 3.0);
+//
+//        transferService.transfer("okex", "ZIL", "100", 3.0);
+//        transferService.transfer("okex", "GRT", "10", 3.0);
+//        transferService.transfer("okex", "EOS", "2", 3.0);
+//        transferService.transfer("okex", "XEM", "30", 3.0);
+//
+//        transferService.transfer("okexsub1", "BSV", "0.05", 3.0);
+//        transferService.transfer("okex", "DYDX", "5", 3.0);
+//        transferService.transfer("okex", "FTM", "10", 3.0);
+//        transferService.transfer("okex", "OP", "5", 3.0);
+//        transferService.transfer("okex", "APT", "1", 3.0);
+//        transferService.transfer("okex", "BNB", "0.05", 3.0);
+//        transferService.transfer("okex", "TON", "10", 3.0);
 
 
         transferService.transfer("okexsub2", "SOL", "1", 3.0);
         transferService.transfer("okexsub2", "MATIC", "10", 3.0);
-        transferService.transfer("okex", "ARB", "20", 3.0);
-        transferService.transfer("okex", "BLUR", "20", 3.0);
-        transferService.transfer("okex", "SUI", "10", 3.0);
+//        transferService.transfer("okex", "ARB", "20", 3.0);
+//        transferService.transfer("okex", "BLUR", "20", 3.0);
+//        transferService.transfer("okex", "SUI", "10", 3.0);
 
     }
 }
