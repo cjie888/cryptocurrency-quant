f596c3a5f26888906d932a036991f5a2d2906883
胡成杰 <cjie888@163.com>
2024-02-21 20:07:20 +0800

    change config

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SpotV5Task.java
@@ -36,7 +36,7 @@ public class SpotV5Task {
 //        spotService.netGrid("okex", "OP-USDT", "5", 0.03);
 //        spotService.netGrid("okex", "THETA-USDT", "5", 0.03);
 //        spotService.netGrid("okex", "APT-USDT", "1", 0.03);
-//        spotService.netGrid("okex", "BNB-USDT", "0.05", 0.03);
+        spotService.netGrid("okex", "BNB-USDT", "0.05", 0.03);
 //        spotService.netGrid("okex", "TON-USDT", "10", 0.03);
         spotService.netGrid("okex", "FTM-USDT", "10", 0.03);
 //        spotService.netGrid("okex", "ARB-USDT", "20", 0.03);
