1a14666f4fd262b5f9e71ef708eccdc2eafd6b36
胡成杰 <cjie888@163.com>
2025-05-29 11:55:32 +0800

    change message

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/DexTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/DexTask.java
@@ -7,8 +7,8 @@ import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
 
-@Component
-@Slf4j
+//@Component
+//@Slf4j
 public class DexTask {
 
     @Autowired
@@ -17,8 +17,8 @@ public class DexTask {
     @Scheduled(cron = "7/17 * * * * ?")
     public  void netGrid() {
 //        dexService.sell("501", "85cQsFgbi8mBZxiPppbpPXuV7j1hA8tBwhjF4gKW6mHg", new BigDecimal("0.00004"),new BigDecimal("1000000"));
-        dexService.sell("501", "721VwRWjDbEDMvaKr3y1TQhX6YV3zj4cQKTxh7typump", new BigDecimal("0.0004"),new BigDecimal("120000"));
-        dexService.sell("501", "5U5vTXQnpoFY8waWZhVkBGbMin2p5mfd6tuRScK2pump", new BigDecimal("0.0005"),new BigDecimal("100000"));
+//        dexService.sell("501", "721VwRWjDbEDMvaKr3y1TQhX6YV3zj4cQKTxh7typump", new BigDecimal("0.0004"),new BigDecimal("120000"));
+//        dexService.sell("501", "5U5vTXQnpoFY8waWZhVkBGbMin2p5mfd6tuRScK2pump", new BigDecimal("0.0005"),new BigDecimal("100000"));
 
     }
 }
