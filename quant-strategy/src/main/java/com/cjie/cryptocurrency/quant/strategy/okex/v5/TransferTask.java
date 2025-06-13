0f7f81594a5f23a103db3c24f0791c4ff093854a
胡成杰 <cjie888@163.com>
2025-06-13 11:34:58 +0800

    change message

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/TransferTask.java
@@ -21,7 +21,6 @@ public class TransferTask {
 //        transferService.transfer("okexsub2", "MANA", "1", 3.0);
         transferService.transfer("okex", "DOGE", "100", 3.0);
         transferService.transfer("okex", "OKB", "0.5", 3.0);
-        transferService.transfer("okexsub2", "ETH", "0.01", 3.0);
         transferService.transfer("okex", "AVAX", "0.5", 3.0);
 //        transferService.transfer("okex", "BTC", "0.0005", 3.0);
 //        transferService.transfer("okexsub1", "ETC", "1", 3.0);
@@ -61,19 +60,22 @@ public class TransferTask {
         transferService.transfer("okex", "BNB", "0.05", 3.0);
         transferService.transfer("okex", "TON", "2", 3.0);
 
-
-        transferService.transfer("okexsub2", "SOL", "0.1", 3.0);
 //        transferService.transfer("okexsub2", "MATIC", "10", 3.0);
 //        transferService.transfer("okex", "ARB", "20", 3.0);
 //        transferService.transfer("okex", "BLUR", "20", 3.0);
         transferService.transfer("okex", "SUI", "10", 3.0);
         transferService.transfer("okex", "ICX", "10", 3.0);
 
+        transferService.transfer("okex", "ONDO", "10", 3.0);
 
+    }
+
+    @Scheduled(cron = "29 52 */1 * * ?")
+    public void transfer2() {
+        transferService.transfer("okexsub2", "ETH", "0.01", 3.0);
+        transferService.transfer("okexsub2", "SOL", "0.1", 3.0);
         transferService.transfer("okexsub2", "SHIB", "400000", 3.0);
         transferService.transfer("okexsub2", "TRX", "40", 3.0);
         transferService.transfer("okexsub2", "HBAR", "50", 3.0);
-        transferService.transfer("okex", "ONDO", "10", 3.0);
-
     }
 }
