533525a9fee05a3972f6a82297c3110ba4d75ead
胡成杰 <cjie888@163.com>
2025-06-24 14:10:31 +0800

    change message

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/SpotTask.java
@@ -4,7 +4,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-import sun.awt.Symbol;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
