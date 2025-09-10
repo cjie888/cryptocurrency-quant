08166b0cfdd82c2fcf111f62f5b3792705c1b5a1
胡成杰 <cjie888@163.com>
2025-09-10 15:42:15 +0800

    change message

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MonitorTask.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/MonitorTask.java
@@ -1,10 +1,16 @@
 package com.cjie.cryptocurrency.quant.strategy;
 
+import com.cjie.cryptocurrency.quant.strategy.binance.BinanceFundingRateChecker;
 import com.cjie.cryptocurrency.quant.strategy.okex.v5.OptionsService;
+import com.cjie.cryptocurrency.quant.strategy.okex.v5.SwapV5Service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.temporal.TemporalAdjusters;
 
 @Component
 @Slf4j
@@ -16,7 +22,12 @@ public class MonitorTask {
     @Autowired
     private UpbbitService upbbitService;
 
-    @Scheduled(cron = "3 10 * * * ?")
+    @Autowired
+    private SwapV5Service swapV5Service;
+
+    @Autowired
+    private BinanceFundingRateChecker binanceFundingRateChecker;
+//    @Scheduled(cron = "3 10 * * * ?")
     public void monitorOptionsIV() {
         optionsService.monitorIV("okex", "BTC-USD-250926-100000-C", "BTC", "250926");
         optionsService.monitorIV("okex", "BTC-USD-250926-110000-C", "BTC", "250926");
@@ -33,7 +44,7 @@ public class MonitorTask {
 
     }
 
-   @Scheduled(cron = "3 13 * * * ?")
+//   @Scheduled(cron = "3 13 * * * ?")
     public void monitorOptionsIVSkew() {
         optionsService.monitorIvSkew("okex", "BTC");
         optionsService.monitorIvSkew("okex", "ETH");
@@ -44,5 +55,19 @@ public class MonitorTask {
     public void monitorUpbitNewCoin() {
         upbbitService.monitorNewCoin();
     }
+
+//    @Scheduled(cron = "37 53 * * * ?")
+    public void butterfly() {
+//        optionsService.ironCondor("okexmock1", "BTC", 1, 4, 0.03, 0.01);
+//        optionsService.ironCondor("okexmock1", "ETH", 1, 4, 0.03, 0.01);
+        optionsService.dynamicDeltaHedging("okex", "ETH-USDT-SWAP", "ETH", 0.03, 10);
+
+    }
+
+    @Scheduled(cron = "37 */10 * * * ?")
+    public void binanceFundingRateChecker() {
+        binanceFundingRateChecker.monitorHighFundingRatePairs();
+    }
+
 
 }
---
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/binance/BinanceFundingRateChecker.java
@@ -0,0 +1,76 @@
+package com.cjie.cryptocurrency.quant.strategy.binance;
+
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.cjie.cryptocurrency.quant.service.MessageService;
+import lombok.extern.slf4j.Slf4j;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+@Slf4j
+public class BinanceFundingRateChecker {
+    private static final String API_URL = "https://fapi.binance.com/fapi/v1/premiumIndex";
+    private static final double HIGH_FUNDING_RATE_THRESHOLD = 1; // 资金费率阈值（百分比）
+
+
+    @Autowired
+    @Qualifier("telegramMessageServiceImpl")
+    private MessageService messageService;
+
+    public void monitorHighFundingRatePairs() {
+        try {
+            List<String> highFundingRatePairs = getHighFundingRatePairs();
+            System.out.println("资金费率较高的交易对：");
+            StringBuilder sb = new StringBuilder();
+            for (String pair : highFundingRatePairs) {
+                sb.append(pair).append("\n");
+            }
+            messageService.sendMonitorMessage("binance资金费率较高的交易对", sb.toString());
+        } catch (IOException e) {
+            System.err.println("请求失败: " + e.getMessage());
+        }
+    }
+
+    private static List<String> getHighFundingRatePairs() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(API_URL)
+                .build();
+
+        List<String> highFundingRatePairs = new ArrayList<>();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (!response.isSuccessful()) {
+                throw new IOException("Unexpected code " + response);
+            }
+
+            String jsonData = response.body().string();
+            JSONArray jsonArray = JSON.parseArray(jsonData);
+
+            for (int i = 0; i < jsonArray.size(); i++) {
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                String symbol = jsonObject.getString("symbol");
+                double fundingRate = jsonObject.getDouble("lastFundingRate") * 100; // 转为百分比
+
+                if (Math.abs(fundingRate) >= HIGH_FUNDING_RATE_THRESHOLD) {
+                    highFundingRatePairs.add(
+                            String.format("交易对: %s, 资金费率: %.4f%%", symbol, fundingRate)
+                    );
+                }
+            }
+        }
+
+        return highFundingRatePairs;
+    }
+}
