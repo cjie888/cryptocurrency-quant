0182f8c7f126081c82bb47f5e979e6ac157420ee
胡成杰 <cjie888@163.com>
2025-12-01 09:29:49 +0800

    change message

--- quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SwapV5Task.java
+++ quant-strategy/src/main/java/com/cjie/cryptocurrency/quant/strategy/okex/v5/SwapV5Task.java
@@ -43,13 +43,13 @@ public class SwapV5Task {
 //        swapService.swapAndSpotHedging("okexsub3", "ETH-USDT-SWAP", "ETH-USDT", 0.03, 0.2);
 //        swapService.swapAndSpotHedging("okexmock1", "XRP-USDT-SWAP", "XRP-USDT", 0.01, 1);
 //        swapService.swapAndSpotHedging("okexmock1", "SUI-USDT-SWAP", "SUI-USDT", 0.01, 10);
-        swapService.swapAndSpotHedging("okexsub3", "SOL-USDT-SWAP", "SOL-USDT", 0.03, 0.3);
+//        swapService.swapAndSpotHedging("okexsub3", "SOL-USDT-SWAP", "SOL-USDT", 0.03, 0.3);
 //        swapService.swapAndSpotHedging("okexsub3", "SUI-USDT-SWAP", "SUI-USDT", 0.03, 20);
 //        swapService.swapAndSpotHedging("okexsub3", "ADA-USDT-SWAP", "ADA-USDT", 0.03, 0.5);
-        swapService.swapAndSpotHedging("okexsub3", "BNB-USDT-SWAP", "BNB-USDT", 0.03, 5);
-        swapService.swapAndSpotHedging("okexsub3", "XRP-USDT-SWAP", "XRP-USDT", 0.03, 0.2);
+//        swapService.swapAndSpotHedging("okexsub3", "BNB-USDT-SWAP", "BNB-USDT", 0.03, 5);
+//        swapService.swapAndSpotHedging("okexsub3", "XRP-USDT-SWAP", "XRP-USDT", 0.03, 0.2);
 //        swapService.swapAndSpotHedging("okexsub3", "LINK-USDT-SWAP", "LINK-USDT", 0.03, 5);
-        swapService.swapAndSpotHedging("okexsub3", "DOGE-USDT-SWAP", "DOGE-USDT", 0.03, 0.3);
+//        swapService.swapAndSpotHedging("okexsub3", "DOGE-USDT-SWAP", "DOGE-USDT", 0.03, 0.3);
 
 
         swapService.swapAndSpotHedging("okexsub3", "XAUT-USDT-SWAP", "XAUT-USDT", 0.02, 20);
